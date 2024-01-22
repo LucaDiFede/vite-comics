7f5c0392dbc4b221fa8db2b75fc48489794a50c9
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2024-01-22 17:42:30 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++
+    }
+  },
+  components: {
+    AppHeader
+  },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <div>
+      <button @click="incrementCount()">
+      Incrementa</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Vite Hello
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+</style>
